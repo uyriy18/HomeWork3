f34d1572d0708e0601b7d9846c262b4a76110521
fanat85 <uyriy18@gmail.com>
2020-07-02 23:41:55 +0300

    Add project files.

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        
+        public class Student
+        {
+            private new int[][] marksArray;
+            private string name;
+            private string surname;
+            private string fathername;
+            private string group;
+            private int age;
+            public void arrayInizialization(int[] adm, int[] prog, int[] disign)
+            {
+                marksArray = new int[3][] { adm, prog, disign };
+            }
+            public void setName(string name)
+            {
+                    if (name.Trim() == "") throw new ArgumentException("Name's field can't be empty");
+                    this.name = name;
+              
+            }
+            public string getName()
+            {
+                return name;
+            }
+            public void setSurname(string surname)
+            {
+                if (surname.Trim() == "") throw new ArgumentException("Surname's field can't be empty");
+                this.surname = surname;
+            }
+            public string getsurName()
+            {
+                return surname;
+            }
+            public void setFathername(string fathername)
+            {
+                if (fathername.Trim() == "") throw new ArgumentException("Fathername's field can't be empty");
+                this.fathername = fathername;
+            }
+            public string getfathername()
+            {
+                return fathername;
+            }
+            public void setGroup(string group)
+            {
+                if (group.Trim() == "") throw new ArgumentException("Group's field can't be empty");
+                this.group = group;
+            }
+            public string getGroup()
+            {
+                return group;
+            }
+            public void setAge(int age)
+            {
+                this.age = age;
+            }
+            public int getAge()
+            {
+                return age;
+            }
+
+          
+             public void printMarks()
+            {
+                Console.WriteLine("Admin marks:");
+                foreach (int i in marksArray[0]) 
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine("\nProgram marks:");
+                foreach (int i in marksArray[1])
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine("\nDisign marks:");
+                foreach (int i in marksArray[2])
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            bool check = false;
+            Student st1 = new Student();
+            Console.WriteLine("Enter studen's name: ");
+            st1.setName(Console.ReadLine());
+            Console.WriteLine("Enter studen's surname: ");
+            st1.setSurname(Console.ReadLine());
+            Console.WriteLine("Enter studen's fathername: ");
+            st1.setFathername(Console.ReadLine());
+            Console.WriteLine("Enter studen's age(digits): ");
+            while (!check)
+            {
+                try
+                {
+                    st1.setAge(int.Parse(Console.ReadLine()));
+                    check = true;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message + "\nIncorrect format, try again");
+                }
+            }
+           
+            Console.WriteLine("Enter studen's group: ");
+            st1.setGroup(Console.ReadLine());
+            st1.arrayInizialization(new int[] { 1, 2, 3, 4, 5 },new int[] { 1, 2, 3, 4, 5, 6, 7, 8},new int[] { 1, 2, 3, 4});
+            Console.WriteLine("Studen's name: " + st1.getName() + "\nStudents surname: " + st1.getsurName() + "\nStudents fathername: " + st1.getfathername() + "\nstudent's age:" + st1.getAge() + "\nStudents group: " + st1.getGroup());
+            st1.printMarks();
+            Console.WriteLine();
+        }
+    } 
+    
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { };
+            string name, model;
+            Console.WriteLine("Enter driver's name :");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter bike's model :");
+            model = Console.ReadLine();
+            Moto mt = new Moto(name,model) ;
+            mt.turnOn();
+            mt.formateSpeedReport(out arr);
+            Console.WriteLine("Speed report : ");
+            foreach(int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+    }
+    public partial class Moto
+    {
+        private static bool breakDown;
+        private static bool turnOnEngine;
+        private int speed;
+        private static int acceleration;
+        private string name;
+        private string model;
+        private int choose;
+        private List<int> speedList = new List<int>();
+        public Moto()
+        {
+            this.name = "NoName";
+            this.model = "garageEdition";
+        }
+        public Moto(string name)
+        {
+            this.name = name;
+            this.model = "garageEdition";
+        }
+        public Moto(string name, string model)
+        {
+            this.name = name;
+            this.model = model;
+        }
+        static Moto()
+        {
+            breakDown = false;
+            turnOnEngine = false;
+            acceleration = 0;
+        }
+        public void turnOn()
+        {
+            Console.WriteLine("You've turned on the engine");
+            turnOnEngine = true;
+            speedUp();
+            while (!breakDown)
+            {
+                Console.WriteLine("Dr dr dr dr dr dr dr dr");
+                printState();
+                speed += acceleration;
+                System.Threading.Thread.Sleep(1000);
+                speedList.Add(speed);
+                if (speed == 180)
+                {
+                   
+                    Console.WriteLine("You driving to fast!!! Slow down, now!");
+                    Console.WriteLine("If you want to down your speed press \"1\"");
+                    try
+                    {
+                        choose = int.Parse(Console.ReadLine());
+                        
+                    }
+                    catch (FormatException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        if (choose == 1) speedDown();
+                        else speedUp();
+                    }
+                }
+                if (speed > 200)
+                {
+                    Console.WriteLine("You have crashed!");
+                    breakDown = true;
+                }
+                else if(speed == 0)
+                {
+                    turnOnEngine = false;
+                    Console.WriteLine("You have cut the engine");
+                    break;
+                }
+               
+            }
+            
+        }
+       
+        public void speedUp()
+        {
+            accelerate();
+        }
+        public void speedDown()
+        {
+            toBrake();
+        }
+        static void accelerate()
+        {
+            acceleration = 10;
+        }
+        static void toBrake()
+        {
+            acceleration = -10;
+        }
+        public void formateSpeedReport(out int [] a)
+        {
+                a = new int[0];
+                Array.Resize(ref a, speedList.Count);
+                speedList.CopyTo(a);
+        }
+
+
+
+
+    }
+    public partial class Moto
+    {
+        private void printState()
+        {
+            Console.WriteLine("Your name : " + name);
+            Console.WriteLine("Bike model : " + model);
+            Console.WriteLine("Is engine turn on : " + turnOnEngine);
+            Console.WriteLine("Your speed : " + speed);
+        }
+    }
+}
